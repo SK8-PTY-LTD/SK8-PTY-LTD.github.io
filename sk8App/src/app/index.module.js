ecf463556b9a2c79d7e5c0bec5235719c19557dd
Jack Surface <133342301@163.co'm>
2016-07-04 01:00:44 +1000

    Removed source file

--- sk8App/src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('sk8App', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
